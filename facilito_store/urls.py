ec2473363a7607c8be31820609507ec448f899be
rogerarmandochanac <armandoroger007@gmail.com>
2025-01-11 00:38:47 -0600

    Logeando usuarios

--- facilito_store/urls.py
+++ facilito_store/urls.py
@@ -20,5 +20,5 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', views.login),
+    path('login/', views.login_view),
 ]
--- facilito_store/views.py
+++ facilito_store/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
-def login(request):
+def login_view(request):
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
+    
+    user = authenticate(username=username, password=password)
+    
+    if user:
+        login(request, user)
     
     return render(request, "users/login.html", {})
