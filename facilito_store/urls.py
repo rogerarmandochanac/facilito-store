040f0c652a73808cde38dbea40b2a4a7cc75d079
rogerarmandochanac <armandoroger007@gmail.com>
2025-01-11 00:45:17 -0600

    redirigiendo al inicio

--- facilito_store/urls.py
+++ facilito_store/urls.py
@@ -19,6 +19,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index_view, name="index"),
+    path('login/', views.login_view, name="login"),
     path('admin/', admin.site.urls),
-    path('login/', views.login_view),
+
 ]
--- facilito_store/views.py
+++ facilito_store/views.py
@@ -1,14 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+
+def index_view(request):
+    return render(request, "index.html", {})
 
 def login_view(request):
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
     
-    user = authenticate(username=username, password=password)
+        user = authenticate(username=username, password=password)
     
-    if user:
-        login(request, user)
+        if user:
+            login(request, user)
+            return redirect("index")
     
     return render(request, "users/login.html", {})
